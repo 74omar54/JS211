e1fbcad137eda9a9dd845ba44745fa3d1643ddeb
74omar54 <omargonzalez7474@gmail.com>
2020-07-30 21:33:10 -0500

    TEST

--- RPS/rps.js
+++ RPS/rps.js
@@ -19,7 +19,7 @@ const rockPaperScissors = (hand1, hand2) => {
 
   // Write code here
   // Use the unit test to see what is expected
-
+dsdfgsdffsdaf
 }
 
 // the first function called in the program to get an input from the user
